bf72cbb7bc8e878d24c39519a3f5bd8a4a57d822
Nikolay Andreychuk <andreychuk@mindbox.ru>
2018-01-27 16:07:41 +0300

    Добавил новый сайт на ASP.NET Core и Angular

---
+++ ElectionStatistics.WebSite/ClientApp/app/app.module.shared.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component';
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { AboutComponent } from './components/about/about.component';
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { CounterComponent } from './components/counter/counter.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        CounterComponent,
+        FetchDataComponent,
+	    AboutComponent
+    ],
+    imports: [
+        CommonModule,
+        HttpModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'about', pathMatch: 'full' },
+            { path: 'about', component: AboutComponent },
+            { path: 'counter', component: CounterComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: '**', redirectTo: 'about' }
+        ])
+    ]
+})
+export class AppModuleShared {
+}
