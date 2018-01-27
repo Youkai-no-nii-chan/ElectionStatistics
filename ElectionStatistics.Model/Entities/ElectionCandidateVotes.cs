6429da49f98c996e7b081567075c77d5a4684ffc
Nikolay Andreychuk <andreychuk@mindbox.ru>
2018-01-27 16:36:12 +0300

    Скопировал модель в проект с EF.Core

---
+++ ElectionStatistics.Model/Entities/ElectionCandidateVotes.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ElectionInfo.Model
+{
+    public class ElectionCandidateVotes
+    {
+        [Key, Column("ElectionResultId", Order = 0)]
+        public int ElectionResultId { get; set; }
+        public virtual ElectionResult ElectionResult { get; set; }
+
+        [Key, Column("CandidateId", Order = 1)]
+        public int CandidateId { get; set; }
+        public virtual Candidate Candidate { get; set; }
+
+        public int Count { get; set; }
+    }
+}
---
+++ ElectionStatistics.Model/QueryableExtensions/ElectionCandidatesVotesQueryableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ElectionInfo.Model
+{
+    public static class ElectionCandidatesVotesQueryableExtensions
+    {
+        public static IQueryable<ElectionCandidateVotes> ByCandidate(
+            this IQueryable<ElectionCandidateVotes> items,
+            Candidate candidate)
+        {
+            if (candidate == null)
+                throw new ArgumentNullException("candidate");
+
+            return items.ByCandidate(candidate.Id);
+        }
+
+        public static IQueryable<ElectionCandidateVotes> ByCandidate(
+            this IQueryable<ElectionCandidateVotes> items,
+            int candidateId)
+        {
+            if (items == null)
+                throw new ArgumentNullException("items");
+
+            return items.Where(result => result.CandidateId == candidateId);
+        } 
+    }
+}
